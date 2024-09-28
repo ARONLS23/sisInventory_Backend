14f9d0142c0de6eac8413d44a7f1235c6762d33e
ARONLS23 <aronsls20@gmail.com>
2024-09-28 11:42:10 -0500

    servicio excel para exportar categorias

--- src/main/java/com/company/inventory/controllers/CategoryController.java
+++ src/main/java/com/company/inventory/controllers/CategoryController.java
@@ -3,9 +3,13 @@ package com.company.inventory.controllers;
 import com.company.inventory.models.Category;
 import com.company.inventory.response.CategoryResponseRest;
 import com.company.inventory.services.ICategoryService;
+import com.company.inventory.util.CategoryExcelExporter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @CrossOrigin("*")
 @RestController
@@ -60,6 +64,23 @@ public class CategoryController {
 
         ResponseEntity<CategoryResponseRest> categoryResponse = categoryService.searchByName(name);
         return categoryResponse;
+
+    }
+
+    @GetMapping("/categories/export/excel")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/octet-stream");
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=result_category.xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        ResponseEntity<CategoryResponseRest> categoryResponse = categoryService.search();
+
+        CategoryExcelExporter excelExporter = new CategoryExcelExporter(categoryResponse.getBody().getCategoryResponse().getCategory());
+
+        excelExporter.export(response);
 
     }
 
